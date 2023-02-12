1cb2099a99cb884e7553581950c0440e16ee31c3
omio-somoy <omio.somoy@northsouth.edu>
2023-02-13 03:52:42 +0600

    ES6 Refresher-1

---
+++ ES6/arrow.js
@@ -0,0 +1,22 @@
+// Normal 
+function retSome(num) {
+    return num*num;
+  }
+console.log(retSome(5));
+
+  // Arrow Function
+  let a = () => {
+    console.log("Omio");
+  };
+  a();
+
+  let b = (name) => {
+    return name;
+  };
+  console.log(b("Somoy"));
+
+  let c = name => name;
+  console.log(c("Hello There!"));
+
+  let saySomething = num => num*num;
+  console.log(saySomething(25));
---
+++ ES6/varriable.js
@@ -0,0 +1,13 @@
+//let
+let b = 30;
+console.log(b);
+b = "Somoy";
+console.log(b);
+//var
+var a = 30;
+console.log(a);
+var a = "Omio";
+console.log(a);
+//const
+const c = 30;
+console.log(c);
